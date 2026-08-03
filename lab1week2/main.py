fac22c55d21ba2d6f6cf5a2c0dd8bbef2706ecac
mada-coding-challenge <webdevsaudi@gmail.com>
2026-08-03 20:09:24 +0300

    python week2 labs

---
+++ lab1week2/main.py
@@ -0,0 +1,17 @@
+Student_name = "Sara"
+student_name = "Abdullah"
+
+print(Student_name)
+print(student_name)
+
+
+##Lab2
+
+
+student_name = "Mada"
+student_age = 30
+
+course = "Python Programming"
+
+print(f"""welcome {student_name} """)
+
