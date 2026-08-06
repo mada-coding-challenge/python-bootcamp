3d3e459dc7dcaf5ba2855f6900f24e185ccb7476
mada-coding-challenge <webdevsaudi@gmail.com>
2026-08-06 19:59:57 +0300

    to lab 5

--- week2day5labs/main.py
+++ week2day5labs/main.py
@@ -1,7 +1,8 @@
+#Lab1
 rangeNums = int(input("Enter number: "))
 
 evenCount = 0
-for number in range(rangeNums):
+for number in range(1, rangeNums + 1):
     if number % 2 == 0:
         print(f"{number} is even")
         evenCount += 1
@@ -10,5 +11,29 @@ for number in range(rangeNums):
 
 print(f"Total even numbers: {evenCount} And total is {rangeNums}")
 
+#Lab1
+for number in range(3):
+    print(f"Attempt: {number + 1}")
 
+#lab2
+
+for num in range(2 , 11 , 2):
+    print(num)
+
+#lab3
+for secnndsToLaunch in range(10, 0, -1):
+    print(f"T-:{secnndsToLaunch}")  
+
+#lab4
+
+course = "Python"
+for letter in course:
+    print(letter)
+
+
+#lab 5
+students = ["sara", "Shahad" , "Khadija"]
+
+for student in students:
+    print(f"Progressing student is:{student}")
 
